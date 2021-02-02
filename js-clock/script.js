41dcf4aca8fee5ca71a09b4b8e4f649bb0b1dc46
Ehsanghaffarii <ghafari.5000@gmail.com>
2021-02-02 21:38:17 +0330

    js clock

---
+++ js-clock/script.js
@@ -0,0 +1,25 @@
+
+const showClock = () => {
+  // get dom-element with id
+  const clock = document.getElementById('clock');
+  // use js Date()
+  let NowDate = new Date();
+
+  // initial hours & minutes & seconds from date
+  let h = NowDate.getHours();
+  let m = NowDate.getMinutes();
+  let s = NowDate.getSeconds();
+
+  // add rules to clock with if segments
+  // use short hand if else
+  m = m < 10 ? `0${m}` : m;
+  s = s < 10 ? `0${s}` : s;
+
+  // add to clock element
+  clock.innerText = `${h}:${m}:${s}`;
+  
+  setTimeout(showClock, 1000);
+  
+}
+
+showClock()
---
+++ textColor/script.js
@@ -0,0 +1,29 @@
+// const categoryElement = document.querySelector('h2');
+
+// const categoryElementValue = categoryElement.textContent;
+
+const jsName = 'جاوااسکریپت'
+const designName = 'طراحی'
+const wpName = 'وردپرس'
+
+// const categoryChange = () => 
+//  categoryElementValue === jsName
+//  ? categoryElementValue
+//  : null;
+
+//   categoryElementValue === wpName
+//  ? categoryElement.classList.add('wpCss') : null;
+
+// categoryChange()
+
+
+
+const categoryColor = () => {
+  
+  const el2 = document.getElementsByTagName('h2');
+  
+
+}
+
+categoryColor()
+
