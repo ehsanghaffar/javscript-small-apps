d6d66e3f3a5e5f69d8a97a188ab5af57a770ce83
Ehsanghaffarii <ghafari.5000@gmail.com>
2021-06-15 15:54:28 +0430

    add regex pattern

---
+++ regex-pattern/script.js
@@ -0,0 +1,24 @@
+
+// program to validate the phone number with regex pattern
+
+function validatePhone(num) {
+
+  // regex pattern for phone number
+  const re = /^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/g;
+
+  // check if the phone number is valid
+  let result = num.match(re);
+  if (result) {
+    console.log(`The number is valid:  ${num}`);
+    
+  }
+  else {
+      let num = prompt('Enter number in XXX-XXX-XXXX format:');
+      validatePhone(num);
+  }
+}
+
+// take input
+let number = prompt('Enter a number XXX-XXX-XXXX');
+
+validatePhone(number);
