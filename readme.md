40bccd6635bb589d4191e3c7e3e4e266910bcc17
ehsanghaffarii <ghafari.5000@gmail.com>
2022-02-17 03:16:48 +0330

    add some short code examples

--- readme.md
+++ readme.md
@@ -9,7 +9,16 @@ My JS beginner project which I created and practice to learn Javascript
 - Password Validate with Javascript
 - Clock with Javascript
 - Javascript Algorithms like selection-sort, bubble-sort & ...
--
+- Javascript Calculator
+- Javascript Calculator with Bootstrap
+- Javascript Email Validation
+- Javascript Hash Password
+- Javascript Letter to uppercase
+- Javascript Password Generator
+- Javascript PalinDrome
+- Javascript Regex pattern
+- Javascript Sort Array
+- Javascript RGB to Hex
 
 #### Description
 
---
+++ short-code-snippets/app.js
@@ -0,0 +1,69 @@
+/* 
+* here some examples of short code
+* for more programming examples check out the github: https://github.com/ehsanghaffarii
+*/
+
+// for loop regular example
+array = [1,2,3,4,5,6,7,8,9,10];
+
+for (var i = 0; i < array.length; i++) {
+  console.log(i);
+}
+  // now with short code ⬇️
+  for (let value of array) {
+    console.log(value);
+  }
+
+
+
+
+// return example in regular way
+dummyMultiply = value => { 
+  return value * value;
+ }
+  // now with short code ⬇️
+  dummyMultiply = value => value * value;
+  // or
+  dummyMultiply = value => { value * value; }
+
+
+
+
+// example of default parameters
+function volume(length, width, height = 1) {
+  if (height === undefined) {
+    height = 1;
+  }
+  if (width === undefined) {
+    width = 1;
+  }
+  return length * width * height;
+}
+  // now with short code ⬇️
+  volume = (length, width, height = 1) => length * width * height;
+
+
+
+
+
+// example of use destructuring
+const name = employee.name
+const age = employee.age
+
+  // now with short code ⬇️
+  const { name, age } = employee;
+
+
+
+
+
+// example of regular function
+function setFullName(firstName, lastName) {
+  return function () {
+    return "Hello I'm" + firstName + " " + lastName;
+  }
+}
+  // now with short code with arrow function & template literal ⬇️
+  const setFullName = (firstName, lastName) => {
+    () => `Hello I'm ${firstName} ${lastName}`;
+  }
