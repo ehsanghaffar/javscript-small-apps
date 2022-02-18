f104e98ca02c173b817bef810b9aa5917627b2b8
ehsanghaffarii <ghafari.5000@gmail.com>
2022-02-18 11:35:28 +0330

    Create UID without package

---
+++ js-tricks/uid.js
@@ -0,0 +1,10 @@
+
+// create uid without any packahge or library in JavaScript
+
+const uid = () => {
+  const firstSection = Date.now().toString(36);
+  const secondSection = Math.random().toString(36).substring(2);
+  return firstSection.concat(secondSection);
+}
+
+console.log(uid());
