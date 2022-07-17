aaead47340a553df953d46d3b7d9c54467faadbb
ehsanghaffarii <ghafari.5000@gmail.com>
2022-07-18 02:34:16 +0430

    some usefull snippets 2

---
+++ quiz/quizq.js
@@ -0,0 +1,9 @@
+/**
+ * Quiz Question Javascript Part 1
+ */
+
+var a = (1, 5 - 1) * 2; // returns 8
+
+var b = (1, 2, 3); // returns 3
+// explanation:
+// comma operator (,) evaluates the each of the operands from left to right and returns the last one.
---
+++ useful-Snippets/copy-to-clipboard.js
@@ -0,0 +1,7 @@
+/**
+ * Copy to clipboard
+ */
+
+const copy = (text) => navigator.clipboard.writeText(text);
+
+copy("Hello World!");
