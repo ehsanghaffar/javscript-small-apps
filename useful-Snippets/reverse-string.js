a9fc65d2f20d3dab1602cf55e3cfbb98ccb6db06
ehsanghaffarii <ghafari.5000@gmail.com>
2022-09-10 17:33:16 +0430

    reverse string

--- useful-Snippets/reverse-string.js
+++ useful-Snippets/reverse-string.js
@@ -2,7 +2,17 @@
  * Reverse a string
  */
 
+// Method 1
 function reverseString(str) {
   return str.split("").reverse().join("");
 }
 console.log(reverseString("hello"));
+
+// Method 2
+function reverse(s) {
+  var o = "";
+  for (var i = s.length - 1; i >= 0; i--) o += s[i];
+  return o;
+}
+
+console.log(reverse("Hello"));
