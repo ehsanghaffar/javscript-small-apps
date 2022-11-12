6d785f7565d4191eaa34a75d0e65085affd03e3c
ehsanghaffarii <ghafari.5000@gmail.com>
2022-11-12 15:25:03 +0330

    string search

---
+++ js-tricks/string-search.js
@@ -0,0 +1,10 @@
+// JS string search
+const simple = "My id is @ehsanghaffar"
+
+const index = simple.search("@ehsanghaffar")
+
+console.log(index) // 9
+
+const secondIndex = simple.search("Developer")
+
+console.log(secondIndex) // -1
