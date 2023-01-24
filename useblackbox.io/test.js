af67d2b78668527acd73bcb47b61fd2223cf04b9
ehsanghaffarii <ghafari.5000@gmail.com>
2023-01-24 18:35:09 +0330

    update useblackbox.io test

--- useblackbox.io/test.js
+++ useblackbox.io/test.js
@@ -1,44 +1,21 @@
 // ask question like below
 
-//connect MYSQL in nodejs?
-var mysql      = require('mysql');
-var connection = mysql.createConnection({
-  host     : 'example.org',
-  user     : 'bob',
-  password : 'secret',
-});
-
-connection.connect(function(err) {
-  // connected! (unless `err` is set)
-});
-
-
-var post  = {id: 1, title: 'Hello MySQL'};
-var query = connection.query('INSERT INTO posts SET ?', post, function(err, result) {
-  // Neat!
-});
-console.log(query.sql); // INSERT INTO posts SET `id` = 1, `title` = 'Hello MySQL'
-
-
 //define middleware in nodejs?
 // express
 var app = express();
 // middleware
 var stack = middleware();
 
-
 // express
 app.use(express.static(..));
 // middleware
 stack.use(function(data, next) {
   next();
-});
-
+}); 
 
 // express, you need to do nothing
 // middleware
 stack.handle(someData);
-
 
 var middleware = require("../src/middleware.js");
 
@@ -116,12 +93,5 @@ app.get("/posts", [
         });
     }
 ], function(req, res) {
-   stack.handle(req, res); 
+  stack.handle(req, res);
 });
-
-
-
-
-//Source: https://stackoverflow.com/questions/7337572
-
-
