fd82333dcb05505b3212e9f442cf24499bd48bc2
ehsanghaffarii <ghafari.5000@gmail.com>
2023-02-22 23:04:27 +0330

    vscode status bar

--- console/log.js
+++ console/log.js
@@ -1,30 +1,25 @@
+let foo = 2,
+  bar = 4,
+  loggedIn = false;
 
-let foo = 2, bar = 4, loggedIn = false
-
-console.log({foo, bar})
-
-
-
+console.log({ foo, bar });
 
 // condition
 // verbose 💩
 if (loggedIn) {
-  console.error('user is logged in assertion failed');
+  console.error("user is logged in assertion failed");
 }
 
-// much better 😍 
-console.assert(loggedIn, 'user is logged in');
-
-
+// much better 😍
+console.assert(loggedIn, "user is logged in");
 
 // Timer in console
 console.time();
 // do stuff
-console.timeLog('did stuff');
+console.timeLog("did stuff");
 // do more stuff
-console.timeLog('did more stuff');
-console.timeEnd()
-
+console.timeLog("did more stuff");
+console.timeEnd();
 
 // add custom style to console
-console.log('%c YOUR_MESSAGE', 'YOUR_CSS')
+console.log("%c YOUR_MESSAGE", "YOUR_CSS");
