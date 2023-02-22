bf349e201473c4344b72411f94097ad204506fb8
ehsanghaffarii <ghafari.5000@gmail.com>
2023-02-22 22:55:37 +0330

    add console methods

---
+++ console/log.js
@@ -0,0 +1,30 @@
+
+let foo = 2, bar = 4, loggedIn = false
+
+console.log({foo, bar})
+
+
+
+
+// condition
+// verbose 💩
+if (loggedIn) {
+  console.error('user is logged in assertion failed');
+}
+
+// much better 😍 
+console.assert(loggedIn, 'user is logged in');
+
+
+
+// Timer in console
+console.time();
+// do stuff
+console.timeLog('did stuff');
+// do more stuff
+console.timeLog('did more stuff');
+console.timeEnd()
+
+
+// add custom style to console
+console.log('%c YOUR_MESSAGE', 'YOUR_CSS')
