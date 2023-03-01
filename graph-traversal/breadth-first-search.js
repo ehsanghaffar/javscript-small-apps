3d525a59fe92ef49384fd54127e0e83be2b2c6f6
ehsanghaffarii <ghafari.5000@gmail.com>
2023-03-01 20:36:00 +0330

    Breadth-first

---
+++ graph-traversal/breadth-first-search.js
@@ -0,0 +1,33 @@
+function bfs(start) {
+
+    const visited = new Set();
+
+    const queue = [start];
+
+
+    while (queue.length > 0) {
+
+        const airport = queue.shift(); // mutates the queue
+
+        const destinations = adjacencyList.get(airport);
+
+
+        for (const destination of destinations) {
+
+            if (destination === 'BKK') {
+                console.log(`BFS found Bangkok!`)
+            }
+
+            if (!visited.has(destination)) {
+                visited.add(destination);
+                queue.push(destination);
+            }
+
+        }
+
+
+    }
+
+}
+
+bfs('PHX')
