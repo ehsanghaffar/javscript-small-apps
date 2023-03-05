0aff08ec00f7c0b4b4aa940a4226322ff46b2dcf
ehsanghaffarii <ghafari.5000@gmail.com>
2023-03-05 08:23:08 +0330

    graph

---
+++ graph-traversal/graph.js
@@ -0,0 +1,34 @@
+// DATA
+const airports = 'PHX BKK OKC JFK LAX MEX EZE HEL LOS LAP LIM'.split(' ');
+
+const routes = [
+    ['PHX', 'LAX'],
+    ['PHX', 'JFK'],
+    ['JFK', 'OKC'],
+    ['JFK', 'HEL'],
+    ['JFK', 'LOS'],
+    ['MEX', 'LAX'],
+    ['MEX', 'BKK'],
+    ['MEX', 'LIM'],
+    ['MEX', 'EZE'],
+    ['LIM', 'BKK'],
+];
+
+
+// The graph
+const adjacencyList = new Map();
+
+// Add node
+function addNode(airport) {
+    adjacencyList.set(airport, []);
+}
+
+// Add edge, undirected
+function addEdge(origin, destination) {
+    adjacencyList.get(origin).push(destination);
+    adjacencyList.get(destination).push(origin);
+}
+
+// Create the Graph
+airports.forEach(addNode);
+routes.forEach(route => addEdge(...route))
