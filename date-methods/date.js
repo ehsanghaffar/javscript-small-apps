6c98a0adf13a270c7b5c343d8e779d696a807638
ehsanghaffarii <ghafari.5000@gmail.com>
2023-03-17 15:15:23 +0330

    date methods fix

--- date-methods/date.js
+++ date-methods/date.js
@@ -29,3 +29,33 @@ console.log(date1); // Wed Jul 01 2020 05:45:00 GMT+0545
 // You can also pass specific time to ISO dates.
 const date = new Date("2020-07-01T12:00:00Z");
 console.log(date); // Wed Jul 01 2020 17:45:00 GMT+0545
+
+
+
+
+// 
+new Date().toDateString(); // e.g. "Fri Nov 11 2016"
+
+new Date().toISOString(); // e.g. "2016-11-21T08:00:00.000Z"
+
+new Date().toJSON(); // e.g. "2016-11-21T08:00:00.000Z"
+
+new Date().toLocaleDateString(); // e.g. "21/11/2016"
+
+new Date().toLocaleString(); // e.g. "21/11/2016, 08:00:00 AM"
+
+new Date().toLocaleTimeString(); // e.g. "08:00:00 AM"
+
+new Date().toString(); // e.g. "Fri Nov 21 2016 08:00:00 GMT+0100 (W. Europe Standard Time)"
+
+new Date().toISOString().slice(0,10); //return YYYY-MM-DD
+
+// Specifying the locale for standard functions:
+
+toLocaleDateString([locales[, options]])
+toLocaleTimeString([locales[, options]])
+toLocaleString([locales[, options]])
+//e.g. toLocaleDateString('ko-KR');
+// toLocaleString('en-GB', { month: 'short' })
+// toLocaleString('en-GB', { month: 'long' })
+// toLocaleString('en-GB', { hour12: true }));
