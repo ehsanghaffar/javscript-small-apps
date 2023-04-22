d2147387663183ca2939b1efab7fb7b536642fac
ehsanghaffarii <ghafari.5000@gmail.com>
2023-04-23 02:56:06 +0330

    email validate with regex

---
+++ regex-pattern/validate-email.js
@@ -0,0 +1,8 @@
+function validateEmail(email) {
+  if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)) {
+    return true;
+  }
+  return false;
+}
+
+console.log(validateEmail("eghg.com"))
--- regex-pattern/validate-phone-number.js
+++ regex-pattern/validate-phone-number.js
@@ -2,15 +2,12 @@
 // program to validate the phone number with regex pattern
 
 function validatePhone(num) {
-
   // regex pattern for phone number
   const re = /^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/g;
-
   // check if the phone number is valid
   let result = num.match(re);
   if (result) {
     console.log(`The number is valid:  ${num}`);
-    
   }
   else {
       let num = prompt('Enter number in XXX-XXX-XXXX format:');
