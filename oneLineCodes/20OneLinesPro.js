f26aa34e7d67216b5f534acf1879d10374f9103c
ehsanghaffarii <ghafari.5000@gmail.com>
2024-03-21 16:30:09 +0330

    Get random boolean, check if date is weekend, check if number is even or odd, get unique values in array, check if variable is array, generate random number between two numbers, generate random string, scroll to top of page, calculate number of days between two dates, copy text to clipboard, different ways of merging multiple arrays, truncate string at end, truncate string from middle, capitalize string, check if user is on Apple device

--- oneLineCodes/20OneLinesPro.js
+++ oneLineCodes/20OneLinesPro.js
@@ -1,9 +1,9 @@
-// Get a random boolean
+/* Get a random boolean */
 const getRandomNumber = () => Math.random() >= 0.5
 console.log(getRandomNumber())
 
 
-// Check if the date is Weekend
+/* Check if the date is Weekend */
 const isWeekend = (date) => [0, 6].indexOf(date.getDay()) !== -1;
 
 console.log(isWeekend(new Date(2021, 4, 14)));
@@ -12,7 +12,7 @@ console.log(isWeekend(new Date(2021, 4, 15)));
 // true (Saturday)
 
 
-// Check if a number is even or odd
+/* Check if a number is even or odd */
 const isEven = (num) => num % 2 === 0;
 
 console.log(isEven(5));
@@ -21,14 +21,14 @@ console.log(isEven(4));
 // true
 
 
-// Get the unique values in an array
+/* Get the unique values in an array */
 const uniqueArr = (arr) => [...new Set(arr)];
 
 console.log(uniqueArr([1, 2, 3, 1, 2, 3, 4, 5]));
 // [1, 2, 3, 4, 5]
 
 
-// Check if a variable is an array
+/* Check if a variable is an array */
 const isArray = (arr) => Array.isArray(arr);
 
 console.log(isArray([1, 2, 3]));
@@ -39,37 +39,37 @@ console.log(isArray('Hello World'));
 // false
 
 
-//  Generate a random number between two numbers
+/*  Generate a random number between two numbers */
 const randomBetweenTwoNumber = (min, max) => Math.floor(Math.random() * (max - min + 1) + min);
 
 console.log(randomBetweenTwoNumber(1, 50));
 
 
-// Generate a random string (unique id?)
+/* Generate a random string (unique id?) */
 const randomString = () => Math.random().toString(36).slice(2);
 
 console.log(randomString());
 
 
-// Scroll to the top of the page
+/* Scroll to the top of the page */
 const scrollToTop = () => window.scrollTo(0, 0);
 
 scrollToTop();
 
 
-// Calculate number of days between two dates
+/* Calculate number of days between two dates */
 const daysDiff = (date, date2) => Math.ceil(Math.abs(date - date2) / 86400000);
 
 console.log(daysDiff(new Date('2021-05-10'), new Date('2021-11-25')));
 
 
-// Copy text to clipboard
+/* Copy text to clipboard */
 const copyTextToClipboard = async (text) => {
   await navigator.clipboard.writeText(text);
 };
 
 
-// Different ways of merging multiple arrays
+/* Different ways of merging multiple arrays */
 // Merge without remove the duplications
 const merge = (a, b) => a.concat(b);
 // Or
@@ -81,7 +81,7 @@ const merge = [...new Set(a.concat(b))];
 const merge = [...new Set([...a, ...b])];
 
 
-// Truncate string at the end
+/* Truncate string at the end */
 const truncateString = (string, length) => {
   return string.length < length ? string : `${string.slice(0, length - 3)}...`;
 };
@@ -92,7 +92,7 @@ console.log(
 // Hi, I should be truncated because...
 
 
-// Truncate string from the middle
+/* Truncate string from the middle */
 const truncateStringMiddle = (string, length, start, end) => {
   return `${string.slice(0, start)}...${string.slice(string.length - end)}`;
 };
@@ -108,14 +108,14 @@ console.log(
 // A long story ... eventually ends!
 
 
-// Capitalizing a string
+/* Capitalizing a string */
 const capitalize = (str) => str.charAt(0).toUpperCase() + str.slice(1);
 
 console.log(capitalize('hello world'));
 // Hello world
 
 
-// Check if the user is on an Apple device
+/* Check if the user is on an Apple device */
 const isAppleDevice = () => /Mac|iPod|iPhone|iPad/.test(navigator.platform);
 
 console.log(isAppleDevice);
